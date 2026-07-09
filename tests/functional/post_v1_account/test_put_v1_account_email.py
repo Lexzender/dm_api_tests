e7ef8d017013d802ad03cb83aad9185bc84a9f41
Aleksey <aleksejj.kostromin@vitech.team>
2026-07-09 20:27:06 +0400

    добавил проверку статуса 403 в тесте test_change_email

--- tests/functional/post_v1_account/test_put_v1_account_email.py
+++ tests/functional/post_v1_account/test_put_v1_account_email.py
@@ -1,3 +1,6 @@
+from checkers.http_checkers import check_status_code_http
+
+
 def test_change_email(account_helper, prepare_user):
     login = prepare_user.login
     password = prepare_user.password
@@ -20,12 +23,11 @@ def test_change_email(account_helper, prepare_user):
         new_email=new_email,
         password=password
     )
-
-    response = account_helper.user_login(
-        login=login,
-        password=password
+    with check_status_code_http(403, "User is inactive. Address the technical support for more details"):
+        response = account_helper.user_login(
+            login=login,
+            password=password
     )
-    assert response.status_code == 403, "Пользователь смог авторизоваться"
 
     account_helper.activate_user(login=login)
 
