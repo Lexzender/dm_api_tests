daaf338c94194958559a5008adbcb1633aa412af
Aleksey <aleksejj.kostromin@vitech.team>
2026-07-09 17:21:29 +0400

    Добавить проверки PyHamcrest в позитивный тест метода get_v1_account

--- tests/functional/get_v1_account/test_get_v1_account.py
+++ tests/functional/get_v1_account/test_get_v1_account.py
@@ -1,2 +1,19 @@
+from hamcrest import (
+    assert_that,
+    all_of,
+    has_property,
+    starts_with,
+    has_properties,
+    equal_to
+)
+
+
 def test_get_v1_account(auth_account_helper):
-    auth_account_helper.dm_account_api.account_api.get_v1_account()
+    response = auth_account_helper.dm_account_api.account_api.get_v1_account()
+    assert_that(response, all_of(
+        has_property("resource", has_property("login", starts_with("Kostromin"))),
+        has_property(
+            "resource", has_properties("roles", ["Guest","Player"])
+        )
+
+    ))
--- tests/functional/post_v1_account/test_post_v1_account.py
+++ tests/functional/post_v1_account/test_post_v1_account.py
@@ -1,5 +1,20 @@
-def test_post_v1_account(account_helper,prepare_user):
+from datetime import datetime
 
+from hamcrest import (
+    assert_that,
+    has_property,
+    starts_with,
+    all_of,
+    instance_of,
+    has_properties,
+    equal_to
+)
+
+
+def test_post_v1_account(
+        account_helper,
+        prepare_user
+):
     login = prepare_user.login
     password = prepare_user.password
     email = prepare_user.email
@@ -8,10 +23,29 @@ def test_post_v1_account(account_helper,prepare_user):
         login=login,
         email=email,
         password=password
-        )
+    )
     response = account_helper.user_login(
         login=login,
-        password=password
-        )
+        password=password,
+        validate_response=True
+    )
 
-    assert response.status_code == 200, "Пользователь не смог авторизоваться"
+    assert_that(
+        response, all_of(
+            has_property("resource", has_property("login", starts_with("Kostromin"))),
+            has_property("resource", has_property("registration", instance_of(datetime))),
+            has_property(
+                "resource", has_properties(
+                    {
+                        "rating":has_properties(
+                            {
+                                "enabled": equal_to(True),
+                                "quality": equal_to(0),
+                                "quantity": equal_to(0),
+                            }
+                        )
+                    }
+                )
+            )
+        )
+    )
